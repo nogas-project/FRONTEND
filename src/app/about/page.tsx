aa46663793da3cc6c328019b657d9702b9e69bd5
hodux <hoduxs@gmail.com>
2025-02-06 13:32:03 -0500

    adds about page & the style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,39 @@
+import styles from "./page.module.css";
+
 export default function About() {
 	return (
-		<div>
-			About
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<ol>
+					<li className={'text-xl'}>
+						About
+					</li>
+					{/* English */}
+					<li className={'text-md'}>
+						Nogas, the gas warning system, is a project made to combat against deadly CO2 contents.
+						It does this with a gas analog sensor, equipped on a Raspberry Pi. This allows us to
+						display a history of the room's air contents and warn the user whether or not they
+						are in danger, and if they need to evacuate.
+					</li>
+					{/* French */}
+					<li className={'text-md'}>
+						Nogas, le système d'alerte gaz, est un projet réalisé pour lutter contre les teneurs mortelles en CO2.
+						Il le fait grâce à un capteur analogique de gaz, équipé sur un Raspberry Pi. Cela nous permet d'afficher
+						un historique du contenu de l'air de la pièce et avertir l'utilisateur s'il est ou non
+						en danger et s'il doit évacuer.
+					</li>
+				</ol>
+			</main>
+			<footer className={styles.footer}>
+				<ol>
+					<li className={'text-lg text-center font-bold'}>
+						<a href={'/..'}> Go back </a>
+					</li>
+					<li className={'text-lg'}>
+						Created in part by Yves-Shaheem Shedid & Mathieu Gouveia Sousa.
+					</li>
+				</ol>
+			</footer>
 		</div>
 	)
 }
