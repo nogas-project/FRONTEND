6004aa002b1affdd2f5eace84152f0eef38f9c5f
hodux <hoduxs@gmail.com>
2025-02-12 21:20:51 -0500

    adds error handling, logic and css

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,36 @@
 "use client"
 import styles from "./page.module.css"
-import {useState} from "react";
+import {FormEvent, useState} from "react";
 import Image from "next/image";
+import { jwtDecode } from "jwt-decode";
 
 export default function Profile() {
 
-    // todo : will use PUT api endpoint
-
+    // todo : add navbar
     /* todo : this will be a protected route requiring authentication */
-
     const [isEditing, setIsEditing] = useState(false)
 
     const [firstName, setFirstName] = useState("joe")
     const [lastName, setLastName] = useState("doe")
     const [email, setEmail] = useState("joe@doe.com")
-    const [phone, setPhone] = useState("514")
-    const [password, setPassword] = useState("abcdefgh")
+    const [phone, setPhone] = useState("514-415-5555")
+    const [password, setPassword] = useState("abcd-1234")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    async function loadData() {
+        const token = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("token"))
+            ?.split("=")[1];
+        
+        if (token) {
+            const decoded = jwtDecode(token);
+            console.log("token", decoded);
+        }
+
+    }
+    loadData()
+
     const profileData = {
         first_name : firstName,
         last_name : lastName,
@@ -26,10 +41,113 @@ export default function Profile() {
 
     // Contacts are obtained via a different route, using the userId
     const [contacts, setContacts] = useState(
-        [{'id': 5, 'name': 'the_Devil', 'phone' : '666'}, {'id': 7, 'name': 'the_Devil', 'phone' : '666'}]
+        [{'id': 5, 'name': 'the_Devil', 'phone' : '666-666-6666'}, {'id': 7, 'name': 'the_Devil', 'phone' : '666-666-6666'}]
     )
 
+    // Handle input verification
+    const phoneRegex : RegExp = /^\d{3}-\d{3}-\d{4}$/;
+    const emailRegex : RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+    const [phoneError, setPhoneError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [contactsError, setContactsError] = useState("");
+    const [contactsNameError, setContactsNameError] = useState("");
+    const [contactsPhoneError, setContactsPhoneError] = useState("");
+    const [serverError, setServerError] = useState("");
+
+    // Repurposed code from register page
     async function handleSubmit() {
+        let isValid = true;
+
+        if (!firstName) {
+            isValid = false;
+            setFirstNameError("You need to enter your first name");
+        } else {
+            setFirstNameError("");
+        }
+
+        if (!lastName) {
+            isValid = false;
+            setLastNameError("You need to enter your last name");
+        } else {
+            setLastNameError("");
+        }
+
+        if (!emailRegex.test(email)) {
+            isValid = false;
+            setEmailError("Your email should look like this: example@email.com");
+        } else {
+            setEmailError("");
+        }
+
+        if (!phoneRegex.test(phone)) {
+            isValid = false;
+            setPhoneError("Your phone number should look like this: 555-555-5555");
+        } else {
+            setPhoneError("");
+        }
+
+        if (password.length < 8) {
+            isValid = false;
+            setPasswordError("Your password must be at least 8 characters");
+        } else {
+            setPasswordError("");
+        }
+
+        if (confirmPassword !== password) {
+            isValid = false;
+            setConfirmPasswordError("Your password doesn't match");
+        } else {
+            setConfirmPasswordError("");
+        }
+
+        // Checks if there's at least one emergency contact, if they all have names,
+        // and if they all have proper phone numbers
+        let hasInvalidPhone = false;
+        let hasInvalidName = false;
+        if (!contacts[0].name && !contacts[0].phone) {
+            setContactsError("Make sure to have at least one emergency contact")
+            isValid = false;
+        } else {
+            setContactsError("");
+        }
+        for (let i = 0; i < contacts.length; i++) {
+            if (!contacts[i].name) {
+                isValid = false;
+                hasInvalidName = true;
+                break;
+            }
+            if (!phoneRegex.test(contacts[i].phone)) {
+                isValid = false;
+                hasInvalidPhone = true;
+                break;
+            }
+        }
+        hasInvalidName ? setContactsNameError("One of your contacts' name is missing") : setContactsNameError("");
+        hasInvalidPhone ? setContactsPhoneError("One of your contacts' phone number is missing or incorrect, it should look like this: 555-555-5555") : setContactsPhoneError("");
+
+        if (!isValid) {
+            console.log("Validation failed. Please fix the errors.");
+            return;
+        }
+
+        const port = process.env.BE_PORT || 3001;
+        try {
+
+            const response = await fetch(`http://localhost:${port}/contacts/userid`, {
+                method: "PUT",
+                headers: {
+                    ContentType: "application/json",
+                },
+                body: JSON.stringify(profileData)
+            })
+
+        } catch (error) {
+
+        }
 
         setIsEditing(!isEditing)
         console.log(contacts)
@@ -56,25 +174,40 @@ export default function Profile() {
                             <input
                                 placeholder={profileData.email}
                                 onChange={e => setEmail(e.target.value)}/>
+                            <li>{emailError}</li>
                             <li className={styles.heading}>Your first name:</li>
                             <input
                                 placeholder={profileData.first_name}
                                 onChange={e => setFirstName(e.target.value)}/>
+                            <li>{firstNameError}</li>
                             <li className={styles.heading}>Your last name:</li>
                             <input
                                 placeholder={profileData.last_name}
                                 onChange={e => setLastName(e.target.value)}/>
+                            <li>{lastNameError}</li>
                             <li className={styles.heading}>Your phone number:</li>
                             <input
                                 placeholder={profileData.phone}
                                 onChange={e => setPhone(e.target.value)}/>
+                            <li>{phoneError}</li>
                             <li className={styles.heading}>Your password:</li>
                             {/* Pop up to inform the password is changed would be nice */}
                             <input
                                 placeholder="Enter new password"
                                 type="password"
                                 onChange={e => setPassword(e.target.value)}/>
+                            <li>{passwordError}</li>
+                            <li>
+                                <input
+                                placeholder="Confirm new password"
+                                type="password"
+                                onChange={e => setConfirmPassword(e.target.value)}/>
+                                <ol>{confirmPasswordError}</ol>
+                            </li>
+
                             <li className={styles.heading}>Your emergency contacts:</li>
+                            <li>{contactsNameError}</li>
+                            <li>{contactsPhoneError}</li>
                             {contacts.map(contact => (
                                 <div key={contact.id}>
                                     <ul>
@@ -101,7 +234,7 @@ export default function Profile() {
                             <li className={styles.heading}>Your phone number:</li>
                             <li>{profileData.phone}</li>
                             <li className={styles.heading}>Your password:</li>
-                            <li>****</li>
+                            <li>********</li>
                             <li className={styles.heading}>Your emergency contacts:</li>
                             <li>
                                 {contacts.map(contact => (
