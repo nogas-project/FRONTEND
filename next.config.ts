3ee74b85c114bcf410ff99ff1ade011535c8be08
Yves-Shaheem <shaheemshedid4@gmail.com>
2025-03-03 21:22:00 -0500

    add BE_URL in the env file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     env:{
-        BE_URL: process.env.BE_URL || "http://nogas.ddns.net:5000",
+        BE_URL: process.env.BE_URL || "https://nogas.ddns.net/api",
     }
 };
 
