700eaf3ab84544f7447400fee8bfef6ee99dd0ee
Yves-Shaheem Shedid <2233669@collegemv.qc.ca>
2025-03-06 16:13:02 -0500

    modified home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,12 +5,13 @@ import dynamic from "next/dynamic";
 import {useEffect, useState} from "react";
 import { Toaster, toast } from "sonner";
 import {validateToken} from "../../../lib/auth.lib";
+import ClipLoader from "react-spinners/ClipLoader";
 const GaugeComponent = dynamic(() => import('react-gauge-component'), { ssr: false });
-
 export default function Home() {
     let token: any;
     let userId: any;
-    const [co, setCo] = useState(0);
+    let [loading1, setLoading1] = useState(false);
+    const [co, setCo] = useState(0.000);
     const [history, setHistory] = useState([{
         co2_amount: 0,
         timestamp: 0
@@ -33,11 +34,8 @@ export default function Home() {
     async function getLatestData() {
         token = getToken()!;
         if (token) {
-            const decoded = validateToken(token);
-
-            // @ts-ignore
+            const decoded = await validateToken(token);
             userId = decoded.id
-
             const response = await fetch(`http://localhost:3001/latest/${userId}`, {
                 method: "GET",
                 headers: {
@@ -57,8 +55,6 @@ export default function Home() {
         token = getToken()!;
         if (token) {
             const decoded = await validateToken(token);
-
-            // @ts-ignore
             userId = decoded.id
 
             const response = await fetch(`http://localhost:3001/history/${userId}`, {
@@ -101,18 +97,18 @@ export default function Home() {
         }
     }
     useEffect(() => {
+        setLoading1(true)
         const interval0 = setInterval(async () => {
             const res = await getLatestData();
             setCo(res.co2_amount);
+            toast.info('Data retrieved')
         }, 30000);
         const interval1 = setInterval(async () => {
             const res = await getDataHistory();
             if (Array.isArray(res))setHistory(res);
         }, 60000);
-
-        return () => {clearInterval(interval0);clearInterval(interval1);};
+        return () => {clearInterval(interval0);clearInterval(interval1); setLoading1(false)};
     }, []);
-
     useEffect(() => {
         const interval = setInterval(async () => {
             if(co > 200){
@@ -124,28 +120,34 @@ export default function Home() {
         }, 15000);
         return () => clearInterval(interval);
     }, [co]);
-
+    const handleRefresh = () => {
+        setLoading1(true);
+        getLatestData().then(data =>setCo(data.co2_amount))
+        getDataHistory().then(data => {if(Array.isArray(data)) setHistory(data)});
+        toast.info('Data retrieved')
+        setLoading1(false);
+    }
     const message = (value: number) => {
         if(value < 100)return "No danger !"
         else if(value < 200) return "Normal"
-        else if(value  < 400) {
-
-            return "Be careful, danger !"
-        }
+        else if(value  < 400) return "Be careful, danger !"
         else if(value  < 800) return "Evacuate immediately !"
         else if (value  < 1600) return "Death in couple hours !"
         else if (value  > 3200) return "Imminent death !"
     }
-
-
     return (
         <div>
             <Toaster />
             <Navbar />
+            <div className={'justify-center'}>
+                <button className="hover:cursor-pointer" onClick={handleRefresh}> Refresh </button>
+                <ClipLoader loading={loading1} aria-label="Loading Spinner" data-testid="loader" size={20} color="#FF0000"/>
+            </div>
+            
             <main className={'flex md:flex-row min-h-svh text-white sm:flex-col sm:gap-5 sm:mt-5'}>
-
-                {/* realtime */}
-                <div className={' flex flex-col w-full items-center justify-center' }>
+                    
+                    {/* realtime */}
+                    <div className={' flex flex-col w-full items-center justify-center' }>
                     <div className={"bg-zinc-900/60 border-zinc-900 rounded-lg m-10 flex flex-col gap-5 m-px "}>
                         <h3 className={'text-3xl text-center mb-10'}> == {message(co)} ==</h3>
                         <div data-testid="gauge">
@@ -190,10 +192,10 @@ export default function Home() {
                         </div>
                     </div>
 
-                </div>
+                    </div>
 
-                {/* history */}
-                <div className={' flex flex-col w-full items-center justify-center' }>
+                    {/* history */}
+                    <div className={' flex flex-col w-full items-center justify-center' }>
                     <div className={"bg-zinc-900/60 border-zinc-900 rounded-lg m-10 flex flex-col gap-5 m-px w-1/2 text-center"}>
                         <h1 className={'text-3xl pt-5'}> == History == </h1>
                         <div className="max-h-72 overflow-y-auto">
@@ -218,7 +220,10 @@ export default function Home() {
                         </div>
 
                     </div>
-                </div>
+                    </div>
+
+                    
+            
             </main>
         </div>
 
